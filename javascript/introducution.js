ec785af486297a9a36bb5e32a678ef5ced7f544c
iiitvamsi <bendalamkrishnavamsi@gmail.com>
2023-08-02 16:05:28 +0530

    Batch 7 class tasks

---
+++ javascript/introducution.js
@@ -0,0 +1,8 @@
+var a = 5;
+var b = 10;
+console.log("Sum:", a + b);
+console.log("Substraction", b - a);
+console.log("Multiplication", a * b);
+console.log("Division", b / a);
+
+console.log("Hello" + " " + "World");
